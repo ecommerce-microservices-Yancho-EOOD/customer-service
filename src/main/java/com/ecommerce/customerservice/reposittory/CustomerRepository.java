c1f9f6990e2e8f3213e50ec4e219eaf2d726feb2
MIRIN COMPETITION <101011386+janetheboss@users.noreply.github.com>
2026-09-15 16:56:07 +0300

    Update CustomerRepository.java

--- src/main/java/com/ecommerce/customerservice/reposittory/CustomerRepository.java
+++ src/main/java/com/ecommerce/customerservice/reposittory/CustomerRepository.java
@@ -4,11 +4,13 @@ import com.ecommerce.customerservice.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
 
     Optional<Customer> findByEmail(String email);
